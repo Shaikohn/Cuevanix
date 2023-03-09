9fef787cac4d337a95cda88724320fe87c0d21ba
unknown <shaicokohn@gmail.com>
2023-03-09 15:18:50 -0300

    Deploy

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,9 @@ import { Provider } from 'react-redux';
 import store from "./redux/store/index"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+axios.defaults.baseURL = "https://cuevanix-backend-production.up.railway.app/" | 'http://localhost:3001';
+
 root.render(
   <Provider store={store}>
     <React.StrictMode>
