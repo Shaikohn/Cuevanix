4db5fe7a2ba40150f9b4cf0cb5de5426f5c08b04
Shai Kohn <shaicokohn@gmail.com>
2025-07-18 19:27:57 -0300

    New design for Auth modal

--- client/src/components/Auth/Auth.jsx
+++ client/src/components/Auth/Auth.jsx
@@ -73,112 +73,142 @@ export default function Auth() {
 
     return (
         <div>
-         <button
-  type="button"
-  className="btn btn-outline-info ms-3 px-4 py-2 rounded-pill fw-semibold"
-  onClick={openedModal}
-  data-bs-toggle="tooltip"
-  data-bs-placement="bottom"
-  title="Log in to access your account"
->
-  Sign In
-</button>
+            <button
+                type="button"
+                className="btn btn-outline-info ms-3 px-4 py-2 rounded-pill fw-semibold"
+                onClick={openedModal}
+                data-bs-toggle="tooltip"
+                data-bs-placement="bottom"
+                title="Log in to access your account"
+            >
+                Sign In
+            </button>
             <Modals isOpenModal={isOpenModal} closeModal={closeModal}>
-                <h2> {isSignUp ? 'Sign Up' : 'Sign In'} </h2>
-                <form className="container" onSubmit={handleSubmit} noValidate>
-                    {
-                    isSignUp && (
-                        <div className='d-flex text-center'>
-                            <div className="form-group col-md-4 marginForm ">
-                                <label>Name</label>
-                                <input autoComplete='off' type="text" name="firstName" className="form-control" placeholder="Name" onChange={handleChange}  />
+                <div className="bg-dark text-white p-4 rounded">
+                    <h2 className="text-center mb-4">{isSignUp ? 'Sign Up' : 'Sign In'}</h2>
+                    <form onSubmit={handleSubmit} noValidate>
+                        <div className="row justify-content-center">
+                            {isSignUp && (
+                                <>
+                                    <div className="col-md-5 mb-3">
+                                        <label>Name</label>
+                                        <input
+                                            autoComplete="off"
+                                            type="text"
+                                            name="firstName"
+                                            className="form-control"
+                                            placeholder="Name"
+                                            onChange={handleChange}
+                                        />
+                                    </div>
+                                    <div className="col-md-5 mb-3">
+                                        <label>Lastname</label>
+                                        <input
+                                            autoComplete="off"
+                                            type="text"
+                                            name="lastName"
+                                            className="form-control"
+                                            placeholder="Lastname"
+                                            onChange={handleChange}
+                                        />
+                                    </div>
+                                </>
+                            )}
+                            <div className="col-md-10 mb-3">
+                                <label>Email</label>
+                                <input
+                                    autoComplete="off"
+                                    type="email"
+                                    name="email"
+                                    className="form-control"
+                                    placeholder="Email"
+                                    onChange={handleChange}
+                                />
                             </div>
-                            <div className="form-group col-md-4 ms-4">
-                                <label>Lastname</label>
-                                <input autoComplete='off' type="text" name="lastName" className="form-control" placeholder="Lastname" onChange={handleChange}  />
-                            </div>
-                        </div>
-                        )
-                    }
-                    <div className='d-flex mt-1 mx-auto text-center'>
-                        <div className="form-group col-md-4 marginForm text-center">
-                            <label>Email</label>
-                            <input autoComplete='off' type="email" name="email" className="form-control" placeholder="Email" onChange={handleChange} />
-                        </div>
-                        <div className="form-group col-md-4 ms-4 text-center">
-                            <label>Password</label>
-                            <div className='d-flex'>
-                                <input type={showPassword ? "text" : "password"} name="password" className="form-control" placeholder="Password" onChange={handleChange} />
-                                <button type='button' style={{background:'none', border: 'none'}} onClick={handleShowPassword}>{showPassword ? <BsFillEyeSlashFill /> : <BsEyeFill />}</button>
-                            </div>
-                        </div>
-                    </div>
-                    {
-                    isSignUp && (
-                        <>
-                            <div className="form-group col-md-4 mx-auto text-center mt-2">
-                                <label>Repeat password</label>
-                                <div className='d-flex'>
-                                    <input type="password" name="confirmPassword" className="form-control" placeholder="Password" onChange={handleChange} />
+                            <div className="col-md-10 mb-3">
+                                <label>Password</label>
+                                <div className="input-group">
+                                    <input
+                                        type={showPassword ? "text" : "password"}
+                                        name="password"
+                                        className="form-control"
+                                        placeholder="Password"
+                                        onChange={handleChange}
+                                    />
+                                    <span className="input-group-text bg-secondary border-0 text-white" style={{ cursor: "pointer" }} onClick={handleShowPassword}>
+                                        {showPassword ? <BsFillEyeSlashFill /> : <BsEyeFill />}
+                                    </span>
                                 </div>
                             </div>
-                        </>
-                        )
-                    }
-                    {
-                    loading ? 
-                        <div className='text-center mt-3 mb-3'>
-                            <div className="spinner-border text-primary" role="status">
+                            {isSignUp && (
+                                <div className="col-md-10 mb-3">
+                                    <label>Repeat Password</label>
+                                    <input
+                                        type="password"
+                                        name="confirmPassword"
+                                        className="form-control"
+                                        placeholder="Repeat Password"
+                                        onChange={handleChange}
+                                    />
+                                </div>
+                            )}
+                        </div>
+                        {loading ? (
+                            <div className="text-center my-3">
+                                <div className="spinner-border text-light" role="status"></div>
+                            </div>
+                        ) : (
+                            <div className="text-center mt-4">
+                                <button type="submit" className="btn btn-primary px-5">
+                                    {isSignUp ? 'Sign Up' : 'Sign In'}
+                                </button>
+                            </div>
+                        )}
+                    </form>
+                    {!loading && (
+                        <div className="d-flex justify-content-center mt-3">
+                            <div style={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
+                                <GoogleLogin
+                                    client_id="569222121144-fap8qmrds81cqlvr1kcqdnbn58flam7b.apps.googleusercontent.com"
+                                    onSuccess={googleSuccess}
+                                    onError={() => console.log('Login Failed')}
+                                />
                             </div>
                         </div>
-                    :   <div className='text-center mt-3'>
-                            <button type="submit" className="btn btn-primary">{isSignUp ? 'Sign Up' : 'Sign In'}</button>
+                    )}
+                    {!isSignUp && !loading && (
+                        <div className="text-center mt-3">
+                            <button className="btn btn-link text-info" onClick={switchForgot}>
+                                Forgot password?
+                            </button>
                         </div>
-                    }
-                </form>
-                {
-                loading ? '' :
-                    <GoogleLogin
-                        client_id="569222121144-fap8qmrds81cqlvr1kcqdnbn58flam7b.apps.googleusercontent.com"
-                        onSuccess={googleSuccess}
-                        onError={() => {
-                            console.log('Login Failed');
-                        }}
-                    />
-                }
-                {
-                !isSignUp && (
-                    <>
-                        <button onClick={() => switchForgot()}>Forgot password?</button>
-                    </>
-                )
-                }
-                {
-                forgotPassword === true && !isSignUp ? 
-                    <div>
-                        <form className="container" onSubmit={handlePasswordSubmit} noValidate>
-                            <div className='d-flex mt-1 mx-auto text-center'>
-                                <div className="form-group col-md-4 marginForm text-center">
-                                    <label>Email</label>
-                                    <input autoComplete='off' type="email" name="email" className="form-control" placeholder="Email" onChange={handlePasswordChange} />
-                                </div>
-                                <button type="submit" className="btn btn-primary ms-3 mt-4" style={{height: '40px'}}>Send</button>
+                    )}
+                    {forgotPassword && !isSignUp && (
+                        <form className="row justify-content-center mt-4" onSubmit={handlePasswordSubmit} noValidate>
+                            <div className="col-md-7 mb-2">
+                                <label>Email</label>
+                                <input
+                                    autoComplete="off"
+                                    type="email"
+                                    name="email"
+                                    className="form-control"
+                                    placeholder="Email"
+                                    onChange={handlePasswordChange}
+                                />
+                            </div>
+                            <div className="col-md-3 d-flex align-items-end">
+                                <button type="submit" className="btn btn-primary w-100">Send</button>
                             </div>
                         </form>
-                    </div> : ''
-                }
-                <div className='d-flex'>
-                {
-                loading ? '' :  
-                    <div className='d-flex'>
-                        {
-                            isSignUp ?
-                            <button className='btn btn-warning changeSign' onClick={switchMode}> Already signed up? Sign In </button>
-                            : <button className='btn btn-warning' onClick={switchMode}> Don't have an account? Sign Up </button>
-                        }
-                        <button type="button" className="btn btn-danger closeAuth" onClick={closeModal}>Close</button>
+                    )}
+                    <div className="d-flex justify-content-between align-items-center mt-4">
+                        <button className="btn btn-warning" onClick={switchMode}>
+                            {isSignUp ? 'Already signed up? Sign In' : "Don't have an account? Sign Up"}
+                        </button>
+                        <button type="button" className="btn btn-danger" onClick={closeModal}>
+                            Close
+                        </button>
                     </div>
-                }
                 </div>
             </Modals>
         </div>
